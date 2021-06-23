cd75f3bd466304ad1cb8be5f5606365d3d157c24
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2021-06-23 08:06:06 +0200

    Initial SetUp + Resources

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Oscar Rodriguez Garrucho on 23/6/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
