fe112b33d94d7e606ec7c933c32bba78eca7344b
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2021-06-24 08:04:10 +0200

    Finish detail view layout with nutrients groupbox

--- Fructus/App/FruitDetailView.swift
+++ Fructus/App/FruitDetailView.swift
@@ -18,25 +18,29 @@ struct FruitDetailView: View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(alignment: .center, spacing: 20) {
+                    // HeaderView
                     FruitHeaderView(fruit: fruit)
                     
                     VStack(alignment: .leading, spacing: 20) {
+                        // Title
                         Text(fruit.title)
                             .font(.largeTitle)
                             .fontWeight(.heavy)
                             .foregroundColor(fruit.gradientColors[1])
-                        
+                        // Headline
                         Text(fruit.headline)
                             .font(.headline)
                             .multilineTextAlignment(.leading)
-                        
+                        // Nutrients
+                        FruitNutrientsView(fruit: fruit)
+                        // Subheadline
                         Text("Learn more about \(fruit.title)".uppercased())
                             .fontWeight(.bold)
                             .foregroundColor(fruit.gradientColors[1])
-                        
+                        // Description
                         Text(fruit.description)
                             .multilineTextAlignment(.leading)
-                        
+                        // Link
                         SourceLinkView()
                             .padding(.top, 10)
                             .padding(.bottom, 40)
