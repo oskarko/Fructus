0614d4cddea6fe08331cd06c7e22ad8c6c907f40
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2021-06-25 15:04:58 +0200

    Create an outstanding Settings View - part II

---
+++ Fructus/View/SettingsRowView.swift
@@ -0,0 +1,49 @@
+//
+//  SettingsRowView.swift
+//  Fructus
+//
+//  Created by Oscar Rodriguez Garrucho on 25/6/21.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+    // MARK: - Properties
+    
+    var name: String
+    var content: String? = nil
+    var linkLabel: String? = nil
+    var linkDestination: String? = nil
+
+    // MARK: - Body
+    
+    var body: some View {
+        VStack {
+            Divider().padding(.vertical, 4)
+            HStack {
+                Text(name).foregroundColor(.gray)
+                Spacer()
+                if (content != nil) {
+                    Text(content!)
+                }
+                else if (linkLabel != nil && linkDestination != nil) {
+                    Link(linkLabel!, destination: URL(string: "https://\(linkDestination!)")!)
+                    Image(systemName: "arrow.up.right.square").foregroundColor(.pink)
+                }
+                else {
+                    /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+                }
+            }
+        }
+    }
+}
+
+// MARK: - Preview
+
+struct SettingsRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsRowView(name: "Developer", content: "John / Jane")
+            .previewLayout(.fixed(width: 375, height: 60))
+            .padding()
+    }
+}
